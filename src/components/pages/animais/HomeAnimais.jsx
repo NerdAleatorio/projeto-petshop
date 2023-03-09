98453af8643e99b191611abc5b1e35f1b1f1e75d
Ian Vinícius Vasconcelos <81888827+NerdAleatorio@users.noreply.github.com>
2023-03-08 21:55:13 -0300

    Correção de layout.

--- src/components/pages/animais/HomeAnimais.jsx
+++ src/components/pages/animais/HomeAnimais.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import './HomeAnimais.css'
 
 import Main from "../../template/Main";
+import Produto from '../../items/Produto';
+import Marcas from '../../items/Marcas';
 
 export default props =>
     <Main>
@@ -9,14 +11,62 @@ export default props =>
             <div className="div-welcome">
                 <img alt="img-animal" className="imagem-welcome" src={props.url}></img>
             </div>
+            
+            <div className="header1"><p className="titulo-destaque">Produtos em promoção</p></div>
             <div className="produtos-promocao">
-                <h1 className="header1">Produtos em promoção</h1>
+                <Produto
+                url='https://tanakao.com.br/media/catalog/product/cache/80cc0569059532456037fee68d18b70f/w/h/whiskas_sache_carne_ao_molho_1_.png'
+                nome='Whiskas Sachê' marca='Whiskas' animal='Gatos' porte='Adulto' 
+                tamanho='500G' preco='R$ 15.99' quantidade='1'/>
+                <Produto
+                url='https://tanakao.com.br/media/catalog/product/cache/80cc0569059532456037fee68d18b70f/w/h/whiskas_sache_carne_ao_molho_1_.png'
+                nome='Whiskas Sachê' marca='Whiskas' animal='Gatos' porte='Adulto' 
+                tamanho='500G' preco='R$ 15.99' quantidade='1'/>
+                <Produto
+                url='https://tanakao.com.br/media/catalog/product/cache/80cc0569059532456037fee68d18b70f/w/h/whiskas_sache_carne_ao_molho_1_.png'
+                nome='Whiskas Sachê' marca='Whiskas' animal='Gatos' porte='Adulto' 
+                tamanho='500G' preco='R$ 15.99' quantidade='1'/>
+                <Produto
+                url='https://tanakao.com.br/media/catalog/product/cache/80cc0569059532456037fee68d18b70f/w/h/whiskas_sache_carne_ao_molho_1_.png'
+                nome='Whiskas Sachê' marca='Whiskas' animal='Gatos' porte='Adulto' 
+                tamanho='500G' preco='R$ 15.99' quantidade='1'/>
+                <Produto
+                url='https://tanakao.com.br/media/catalog/product/cache/80cc0569059532456037fee68d18b70f/w/h/whiskas_sache_carne_ao_molho_1_.png'
+                nome='Whiskas Sachê' marca='Whiskas' animal='Gatos' porte='Adulto' 
+                tamanho='500G' preco='R$ 15.99' quantidade='1'/>
             </div>
+            
+            <div className="header2"><p className="titulo-destaque">Todos os produtos</p></div>
             <div className="produtos-animal">
-                <h1 className="header2">Todos os produtos</h1>
+                <Produto
+                url='https://tanakao.com.br/media/catalog/product/cache/80cc0569059532456037fee68d18b70f/w/h/whiskas_sache_carne_ao_molho_1_.png'
+                nome='Whiskas Sachê' marca='Whiskas' animal='Gatos' porte='Adulto' 
+                tamanho='500G' preco='R$ 15.99' quantidade='1'/>
+                <Produto
+                url='https://tanakao.com.br/media/catalog/product/cache/80cc0569059532456037fee68d18b70f/w/h/whiskas_sache_carne_ao_molho_1_.png'
+                nome='Whiskas Sachê' marca='Whiskas' animal='Gatos' porte='Adulto' 
+                tamanho='500G' preco='R$ 15.99' quantidade='1'/>
+                <Produto
+                url='https://tanakao.com.br/media/catalog/product/cache/80cc0569059532456037fee68d18b70f/w/h/whiskas_sache_carne_ao_molho_1_.png'
+                nome='Whiskas Sachê' marca='Whiskas' animal='Gatos' porte='Adulto' 
+                tamanho='500G' preco='R$ 15.99' quantidade='1'/>
+                <Produto
+                url='https://tanakao.com.br/media/catalog/product/cache/80cc0569059532456037fee68d18b70f/w/h/whiskas_sache_carne_ao_molho_1_.png'
+                nome='Whiskas Sachê' marca='Whiskas' animal='Gatos' porte='Adulto' 
+                tamanho='500G' preco='R$ 15.99' quantidade='1'/>
+                <Produto
+                url='https://tanakao.com.br/media/catalog/product/cache/80cc0569059532456037fee68d18b70f/w/h/whiskas_sache_carne_ao_molho_1_.png'
+                nome='Whiskas Sachê' marca='Whiskas' animal='Gatos' porte='Adulto' 
+                tamanho='500G' preco='R$ 15.99' quantidade='1'/>
             </div>
+            
+            <div className="header3"><p className="titulo-destaque">Filtre por marcas</p></div>
             <div className="marcas-animal">
-                <h1 className="header3">Filtre por marcas</h1>
+                <Marcas url='https://agenciakiwee.com.br/wp-content/uploads/2019/01/Wiskas.png'/>
+                <Marcas url='https://i.pinimg.com/originals/71/59/c6/7159c6ca296e20900bf8c49758620d26.png'/>
+                <Marcas url='https://mex.mars.com/sites/g/files/jydpyr496/files/2019-03/Logos_BUSINESS_SEGMENTS_23.png'/>
+                <Marcas url='https://agenciakiwee.com.br/wp-content/uploads/2019/01/Wiskas.png'/>
+                <Marcas url='https://agenciakiwee.com.br/wp-content/uploads/2019/01/Wiskas.png'/>
             </div>
         </div>
     </Main>
